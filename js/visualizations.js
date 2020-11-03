6baf4ba5006ebb67bae7c468814d796b90464fb4
Alex Day <alexday135@gmail.com>
2020-11-03 16:32:38 -0500

    Add network visualizations and quiz logic

--- js/visualizations.js
+++ js/visualizations.js
@@ -1,3 +1,5 @@
+// MINIMUM AVERAGE RETWEET CHART
+
 function barchart() {
     var margin = {
         top: 30,
@@ -72,5 +74,104 @@ function barchart() {
         .attr("y", height + margin.bottom / 2)
         .text("Average Time for First Retweet (minutes)");
 }
-
 barchart();
+
+// TWEET GRAPH KNOWN
+async function tweet_graph(div_id, filename, color) {
+    let data = await d3.json(filename);
+
+    var width = 500
+    var height= 500
+    
+
+    var svg = d3.select(div_id).append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    
+    var nodes = data.nodes; //read from the dataset the nodes of the graph
+    var arcs = data.links; //read from the dataset the arcs of the graph
+
+    var layout = d3.forceSimulation(nodes)
+    .force('center', d3.forceCenter(width/2, height/2))
+    .force('collisions', d3.forceCollide())
+    .force("manyBody", d3.forceManyBody().strength(-10))
+    .force("link", d3.forceLink(arcs).id(d=>d.id).strength(0.1))
+    .force("x", d3.forceX())
+    .force("y", d3.forceY())
+    .on('tick', ticked);
+
+    
+    var edges = svg.append("g")
+                   .selectAll("line")
+                   .data(arcs)
+                   .enter()
+                   .append("line")
+                   .attr("stroke", "black")
+                   .attr("stroke-width", 0.1);
+    
+    var node = svg.append("g")
+                  .selectAll("circle")
+                  .data(nodes)
+                  .enter()
+                  .append("circle")
+                  .attr("fill", color) //set the appropriate color for each node depending on its group
+                  .attr("r", 3);
+     
+    function ticked(){
+      node
+         .attr('cx', d => d.x)
+         .attr('cy', d => d.y)
+      
+      edges
+         .attr("x1", d => d.source.x)
+         .attr("y1", d => d.source.y)
+         .attr("x2", d => d.target.x)
+         .attr("y2", d => d.target.y)
+    }
+    
+    return svg.node() 
+}
+
+tweet_graph("#non-rumor-graph", "/js/688932211714818048.json", "#00b32c");
+tweet_graph("#false-graph", "/js/520338188213444608.json", "#ff0012");
+
+// TWEET GRAPH QUIZ
+const questions = [
+    {
+        type: "Non-Rumor",
+        path: "/js/688932211714818048.json"
+    },
+    {
+        type: "False",
+        path: "/js/520338188213444608.json"
+    }
+]
+let question_number = 0;
+let right = 0
+
+// Seed the graph
+tweet_graph("#network-select-graph", questions[question_number]["path"], "#7851a9");
+
+document.getElementById("next").onclick = function() {
+    // Get user input
+    let user_choice_rb = document.getElementsByName('graph'); 
+    let user_choice = "";
+    for(i = 0; i < user_choice_rb.length; i++) { 
+        if(user_choice_rb[i].checked) 
+            user_choice = user_choice_rb[i].value;
+    } 
+
+    // Check against known
+    if (user_choice == questions[question_number]["type"]) {
+        console.log("Right!");
+        right++;
+    } else {
+        console.log("Wrong!");
+    }
+    question_number++;
+
+    // Replace the graph
+    document.getElementById("network-select-graph").innerHTML = "";
+    tweet_graph("#network-select-graph", questions[question_number]["path"], "#7851a9");
+}
+
